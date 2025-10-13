5572ee65a2bd2e2e88862cbfd775b66de3d86340
aparnakrishna <aparnakrishna.work@gmail.com>
2025-10-13 19:38:22 +0800

    Added a debugger panel onto streamlit UI and added comments to code

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -4,3 +4,5 @@
 # from .utils import your_utility_functions
 
 __all__ = []  # Add your actual function names here when implemented
+from .utils import CLAUSE_HEADER_RE, detect_clause_label_from_text, excerpt, format_with_citations
+__all__ = ["CLAUSE_HEADER_RE", "detect_clause_label_from_text", "excerpt", "format_with_citations"]
